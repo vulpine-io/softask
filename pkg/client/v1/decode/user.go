c74ba521efa0e9f6f260f33714cd716b7306befc
Elizabeth Harper <foxcapade@gmail.com>
2020-06-11 20:57:59 -0400

    wip

--- pkg/client/v1/decode/user.go
+++ pkg/client/v1/decode/user.go
@@ -48,7 +48,7 @@ func (d *dUser) NKeys() int {
 	return 2
 }
 
-func DecodeFullUser(r io.Reader) (users.User, error) {
+func DecodeUser(r io.Reader) (users.User, error) {
 	dec := gojay.NewDecoder(r)
 	defer dec.Release()
 
