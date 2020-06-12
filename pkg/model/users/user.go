f12d017c305c97178f7906bb3bb3ea45a5315d26
Elizabeth Harper <foxcapade@gmail.com>
2020-06-11 20:51:43 -0400

    users

--- pkg/model/users/user.go
+++ pkg/model/users/user.go
@@ -1,10 +1,12 @@
 package users
 
+import "time"
+
 type User interface {
 	Id() int64
 	DisplayName() string
 	Email() string
-
+	Created() time.Time
 }
 
 type MutableUser interface {
@@ -12,6 +14,8 @@ type MutableUser interface {
 
 	SetId(id int64) MutableUser
 	SetDisplayName(name string) MutableUser
+	SetEmail(email string) MutableUser
+	SetCreated(at time.Time) MutableUser
 }
 
 func NewUser() MutableUser {
@@ -19,16 +23,14 @@ func NewUser() MutableUser {
 }
 
 type user struct {
-	id   int64
-	name string
+	id    int64
+	name  string
+	email string
+	created time.Time
 }
 
 func (u *user) Id() int64 {
 	return u.id
-}
-
-func (u *user) DisplayName() string {
-	return u.name
 }
 
 func (u *user) SetId(id int64) MutableUser {
@@ -36,7 +38,29 @@ func (u *user) SetId(id int64) MutableUser {
 	return u
 }
 
+func (u *user) DisplayName() string {
+	return u.name
+}
+
 func (u *user) SetDisplayName(name string) MutableUser {
 	u.name = name
+	return u
+}
+
+func (u *user) Email() string {
+	return u.email
+}
+
+func (u *user) SetEmail(email string) MutableUser {
+	u.email = email
+	return u
+}
+
+func (u *user) Created() time.Time {
+	return u.created
+}
+
+func (u *user) SetCreated(at time.Time) MutableUser {
+	u.created = at
 	return u
 }
